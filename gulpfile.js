74a6f54b7949e96ffac1291a6df0f2d6e3782a4b
Andrey Alekseev <AnderyAlekseev@ya.ru>
2022-06-29 18:07:29 +0700

    готова шапка. заголовок первого экрана

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 "use strict";
-
-const gulp        = require('gulp');
+const {src, dest, watch, parallel, task} = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
@@ -9,7 +8,7 @@ const rename = require("gulp-rename");
 const htmlmin = require('gulp-htmlmin');
 const { on } = require('events');
 
-gulp.task('server', function() {
+task('server', function() {
 
     browserSync({
         server: {
@@ -17,70 +16,58 @@ gulp.task('server', function() {
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+   watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/scss/**/*.+(scss|sass)")
+task('styles', function() {
+    return src("src/scss/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/css"))
+        .pipe(dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/scss/**/*.+(scss|sass|css)").on('change', gulp.parallel('styles'));
-    gulp.watch("src/*.+(php|html)").on('change', gulp.parallel('html'));
-    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
-    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
-    gulp.watch("src/php/**/*").on('all', gulp.parallel('php'));
-  
+task('watch', function() {
+    watch("src/scss/**/*.+(scss|sass|css)").on('change', parallel('styles'));
+    watch("src/*.html").on('change', parallel('html'));
+    watch("src/js/**/*.js").on('change', parallel('scripts'));
+    watch("src/fonts/**/*").on('all', parallel('fonts'));
+    watch("src/icons/**/*").on('all', parallel('icons'));
+    
 });
 
-gulp.task('html', function () {
-    return gulp.src("src/*.+(php|html)")
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"))
-        .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru"));
-});
+task('html', function () {
+    return src("src/*.html")
+        .pipe( htmlmin(  { collapseWhitespace: true }) )
+        .pipe( dest("dist/"));
+        });
 
-gulp.task('scripts', function () {
-    return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"))
-        .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/js"))
+task('scripts', function () {
+    return src("src/js/**/*.js")
+        .pipe(dest("dist/js"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('fonts', function () {
-    return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"))
-        .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/fonts"))
+task('fonts', function () {
+    return src("src/fonts/**/*")
+        .pipe(dest("dist/fonts"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('icons', function () {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"))
-       .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/icons"))
+task('icons', function () {
+    return src("src/icons/**/*")
+        .pipe(dest("dist/icons"))
         .pipe(browserSync.stream());
 });
-gulp.task('image', function () {
-    return gulp.src("src/img/**/*")
-        .pipe(gulp.dest("dist/img"))
-       .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/img"))
-        .pipe(browserSync.stream());
-});
-gulp.task('php', function () {
-    return gulp.src("src/php/**/*.php")
-        .pipe(gulp.dest("dist/php"))
-       .pipe(gulp.dest("C://OpenServer/domains/OrganicFood.ru/php"))
+
+task('image', function () {
+    return src("src/img/**/*")
+        .pipe(dest("dist/img"))
         .pipe(browserSync.stream());
 });
 
 
 
-gulp.task('default', gulp.parallel('watch', /*'server', */'styles', 'scripts', 'fonts', 'icons', 'html','image','php'));
+task('default', parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html','image'));
